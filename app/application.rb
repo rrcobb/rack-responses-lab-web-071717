ae9306a6bd1268f74e157ac3b43d4bf9a2e4b7fb
Rob Cobb <rwcobbjr@gmail.com>
2018-03-28 15:48:12 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,11 @@
+class Application
+  def call(env)
+    res = Rack::Response.new
+    if Time.now.hour < 12
+      res.write "Good Morning!"
+    else
+      res.write "Good Afternoon!"
+    end
+    res.finish
+  end
+end
